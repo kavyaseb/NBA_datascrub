54730280adcbd54dc1361ac8394d1dca2aa4390d
Kavya Sebastian <kavya.sebastian911@gmail.com>
2020-12-30 14:51:46 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import html.parser
+import requests
+import sys
+import textwrap
+
+url = "https://www.basketball-reference.com/leagues/NBA_2021_per_game.html"
+r = requests.get(url)
+data = r.text
+soup = BeautifulSoup(data, 'lxml')
+list = ''
+f = open("NBAoutput.txt", "w")
+##for link in soup.find_all('td'):
+for tr in soup.find('table', id='per_game_stats').find_all('tr', class_='full_table'):
+    td_list = tr.find_all('td')
+    print(td_list[0].text + "," + td_list[1].text + "," + td_list[2].text + "\n")
+    sen = td_list[0].text + "," + td_list[1].text + "," + td_list[2].text + "\n"
+    unicodeData.encode('ascii', 'ignore')
+    f.write(str(sen))
+    # point = str(link)
+    # print(str(link), end="\n")
+    # lil = str(link.text)
+    # f.write(lil)
+    ##    f.write(textwrap.dedent(point).encode(sys.getdefaultencoding()))
+    # f.write("\n")
+f.close()
+
