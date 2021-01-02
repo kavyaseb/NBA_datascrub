fbcda01f7459d97a547539b1c3c7ff6c908d7b49
kavyaseb <65504752+kavyaseb@users.noreply.github.com>
2021-01-01 20:10:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from bs4 import BeautifulSoup
-import html.parser
 import requests
-import sys
-import textwrap
 
 url = "https://www.basketball-reference.com/leagues/NBA_2021_per_game.html"
 r = requests.get(url)
 data = r.text
 soup = BeautifulSoup(data, 'lxml')
 list = ''
-f = open("NBAoutput.txt", "w")
+f = open("NBAoutput.txt", "w", encoding="utf8")
 for tr in soup.find('table', id='per_game_stats').find_all('tr', class_='full_table'):
     td_list = tr.find_all('td')
     print(td_list[0].text + "," + td_list[1].text + "," + td_list[2].text + "\n")
     sen = td_list[0].text + "," + td_list[1].text + "," + td_list[2].text + "\n"
-    unicodeData.encode('ascii', 'ignore')
-    f.write(str(sen))
+    f.write(sen)
 f.close()
 
